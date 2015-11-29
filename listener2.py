b24256a776f464106837b1886401b2935e8df676
vivaldi96 <roblower96@gmail.com>
2015-11-29 22:05:47 +0000

    fixed listener to use pigpio

--- listener2.py
+++ listener2.py
@@ -1,8 +1,13 @@
+import pigpio
 import rospy
 from std_msgs.msg import Int64
 from Tkinter import *
 import RPi.GPIO as GPIO
 import time
+
+
+pi1 = pigpio.pi()
+
 
 def callback(data):
 	# GPIO expects an int, which is notthe same as Int64, 
@@ -12,23 +17,27 @@ def callback(data):
 	tag, value = str.split(stuff) 
 	pin = int(value)
 	if pin == 0:
+		print ('off')
 		off()
 	else:
 		print(pin)
 		on(pin)
 
 def off():
+	pi1.write(17,0)
+	pi1.write(18,0)
 	#eventually make this an aray and loop thought pin
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(11, GPIO.OUT)
-	GPIO.setup(13, GPIO.OUT)
-	GPIO.output(13,GPIO.LOW)
-	GPIO.output(11,GPIO.LOW)
+	#GPIO.setmode(GPIO.BOARD)
+	#GPIO.setup(11, GPIO.OUT)
+	#GPIO.setup(13, GPIO.OUT)
+	#GPIO.output(13,GPIO.LOW)
+	#GPIO.output(11,GPIO.LOW)
 	
 def on(pin):
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(pin, GPIO.OUT)
-	GPIO.output(pin,GPIO.HIGH)
+	pi1.write(pin, 1)
+	#GPIO.setmode(GPIO.BOARD)
+	#GPIO.setup(pin, GPIO.OUT)
+	#GPIO.output(pin,GPIO.HIGH)
 	return
 
 
