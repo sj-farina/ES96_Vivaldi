976400f6f90c0bcbcaf7db9c6fd053962681a2e5
vivaldi96 <roblower96@gmail.com>
2015-12-03 21:36:56 +0000

    moreerrors

--- DriveTheRobot.py
+++ DriveTheRobot.py
@@ -2,7 +2,7 @@ import rospy
 from std_msgs.msg import Int64
 from Tkinter import *
 import time
-from serial import Serial
+#from serial import Serial
 from multiprocessing import Process
 
 
@@ -21,7 +21,7 @@ L = 3
 R = 4
 
 
-#serialPort = Serial('/dev/ttyAMA0', 9600, timeout = 2)
+serialPort = Serial('/dev/ttyAMA0', 9600, timeout = 2)
 cur_stateR = M1S
 cur_stateL = M2S
 next_stateR = M1S
@@ -39,8 +39,10 @@ def kp(event):
 		direction_set(L)
 	elif event.keysym =='Right' :
 		direction_set(R)
-	else :
+	elif event.keysym =='Space' :
 		save_cur_state(0, 0)
+		direction_set(S)
+	else :
 		direction_set(S)
 
 
@@ -68,62 +70,68 @@ def direction_set(direction):
 def update(next_stateR, next_stateL):
 	cur_stateR, cur_stateL = get_cur_state()
 	if (cur_stateR != next_stateR):
-		p1 = Process(target = motor1)
+		p1 = Process(target = motor1, args = (cur_stateR, cur_stateL, next_stateR, next_stateL))
 		p1.start()
 
 	if (cur_stateL != next_stateL):
-		p2 = Process(target = motor2)
+		p2 = Process(target = motor2, args = (cur_stateR, cur_stateL, next_stateR, next_stateL))
 		p2.start()
 
-def motor1(cur_stateR):
+def motor1(cur_stateR, cur_stateL, next_stateR, next_stateL):
 	#first set to 64 
 	print 'start motor1'
 	cur_stateR = M1S
-	refresh(cur_stateL, cur_stateR)
+	refresh(cur_stateR, cur_stateL)
 	time.sleep(1)
+	'''for x in range (cur_stateR, M1S):
+		cur_stateR = x
+		refresh(cur_stateR, cur_stateL)
+		time.sleep(.01)
+	for x in range (cur_stateR, M1S,-1):
+		cur_stateR = x
+		refresh(cur_stateR, cur_stateL)
+		time.sleep(.01)'''
   	# then itterate to the desired stat
 	if next_stateR == M1F:
 		for x in range (M1S, M1F):
 			cur_stateR = x
-			time.sleep(.01)
-			refresh(cur_stateL, cur_stateR)
+			time.sleep(.05)
+			refresh(cur_stateR, cur_stateL)
 	elif next_stateR == M1B:
 		for x in range (M1S, M1B, -1):
 			cur_stateR = x
-			time.sleep(.01)
-			refresh(cur_stateL, cur_stateR)
-	save_cur_state(cur_stateL, cur_stateR)
+			time.sleep(.05)
+			refresh(cur_stateR, cur_stateL)
+	save_cur_state(cur_stateR, cur_stateL)
 	print 'end motor1'
 
 
 
-def motor2(cur_stateL):
+def motor2(cur_stateR, cur_stateL, next_stateR, next_stateL):
 	#first set to 64
 	print 'start motor2'
 	cur_stateL = M2S
-	refresh(cur_stateL, cur_stateR)
+	refresh(cur_stateR, cur_stateL)
 	time.sleep(1)
   	# then itterate to the desired stat
 	if next_stateL == M2F:
 		for x in range (M2S, M2F):
 			cur_stateL = x
-			time.sleep(.01)
-			refresh(cur_stateL, cur_stateR)
+			time.sleep(.05)
+			refresh(cur_stateR, cur_stateL)
 	elif next_stateL == M2B:
 		for x in range (M2S, M2B, -1):
 			cur_stateL = x
-			time.sleep(.01)
-			refresh(cur_stateL, cur_stateR)
-	save_cur_state(cur_stateL, cur_stateR)
+			time.sleep(.05)
+			refresh(cur_stateR, cur_stateL)
+	save_cur_state(cur_stateR, cur_stateL)
 	print 'end motor2'
 
-
-p1 = threading.Thread(target=motor1, args=(cur_stateR))
-p2 = threading.Thread(target=motor2, args=(cur_stateL))
 
 def refresh(cur_stateR, cur_stateL):
 	#serialPort.write(chr(cur_stateR))
 	#serialPort.write(chr(cur_stateL))
+	print '...'
 	print cur_stateR
 	print cur_stateL
 
@@ -131,7 +139,7 @@ def save_cur_state(cur_stateR, cur_stateL):
 	global cur_stateR_save
 	global cur_stateL_save
 	cur_stateR_save = cur_stateR
-	cur_stateL_save	= cur_stateL
+	cur_stateL_save = cur_stateL
 	#print cur_stateL_save
 	#print cur_stateR_save
 
@@ -141,7 +149,7 @@ def get_cur_state():
 	global cur_stateL_save
 	cur_stateR = cur_stateR_save
 	cur_stateL = cur_stateL_save
-	return(cur_stateL, cur_stateR)
+	return(cur_stateR, cur_stateL)
 
 
 main.bind_all('<KeyPress>', kp)
@@ -149,5 +157,5 @@ main.bind_all('<KeyPress>', kp)
 
 main.mainloop()
 
-serialPort.close()
+#serialPort.close()
 
